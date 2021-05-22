0f941689d557d4cdd034b2395ee0c222b7a239e6
Rocio Florez <rocioflor3z@gmail.com>
2021-05-22 18:54:01 -0300

    Added logic and modal

--- code/R3/assets/script.js
+++ code/R3/assets/script.js
@@ -1,23 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() { startMediaPlayer(); }, false);
 
 let mediaPlayer;
+let btn = document.getElementById('play-pause-button');
 
 function startMediaPlayer() {
     mediaPlayer = document.getElementById('video');
     mediaPlayer.controls = false;
 }
 
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".open__modal");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
 function togglePlayPause() {
-    let btn = document.getElementById('play-pause-button');
     if (mediaPlayer.paused || mediaPlayer.ended) {
        btn.title = 'pause';
-       btn.innerHTML = 'pause';
        btn.className = 'pause';
        mediaPlayer.play();
     }
     else {
        btn.title = 'play';
-       btn.innerHTML = 'play';
        btn.className = 'play';
        mediaPlayer.pause();
     }
